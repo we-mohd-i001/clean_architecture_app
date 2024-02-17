2b879217a6f3f10ab58d44b54965010060f6dffc
Mohd Imran <mohd-i001@webreinvent.com>
2024-02-17 11:05:33 +0530

    Added theme service to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
+import 'package:clean_architecture_app/application/services/theme_service.dart';
+import 'package:clean_architecture_app/theme/color_schemes/color_schemes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(create: (context) => ThemeService(), child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Clean Architecture',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-    );
+    return Consumer<ThemeService>(builder:(context, themeService, child){
+      return MaterialApp(
+        title: 'Flutter Demo',
+        theme: themeService.isDarkModeOn
+            ? ThemeData(useMaterial3: true, colorScheme: lightColorScheme)
+            : ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+        // routes: {
+        //   '/': (context) => const HomeScreen(),
+        //   'counter': (context) => const CounterScreen(),
+        //   'listview': (context) => const ListExample(),
+        // },
+        home: const Placeholder(),
+      );
+    });
   }
 }
