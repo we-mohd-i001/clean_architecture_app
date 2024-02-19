f57bd05daf8f424170a9201c464b8a62cac11193
Mohd Imran <mohd-i001@webreinvent.com>
2024-02-19 18:13:11 +0530

    Wrote tests for testing repositories

--- lib/domain/failures/failures.dart
+++ lib/domain/failures/failures.dart
@@ -1,7 +1,21 @@
+import 'package:equatable/equatable.dart';
+
 abstract class Failures{}
 
-class ServerFailure extends Failures{}
+class ServerFailure extends Failures with EquatableMixin{
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
 
-class CacheFailure extends Failures{}
+class CacheFailure extends Failures with EquatableMixin{
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
 
-class GeneralFailure extends Failures{}
+class GeneralFailure extends Failures with EquatableMixin{
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
