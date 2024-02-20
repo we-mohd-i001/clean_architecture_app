752cdb0a77bec0a2ecb9d9772982e31921bc0642
Mohd Imran <mohd-i001@webreinvent.com>
2024-02-20 13:22:53 +0530

    Wrote tests for testing the bloc and business logic
    Tests:
      - bloc test for Adviser Bloc
           - should emit 
                - nothing when no event is triggered
                - when event is triggered
                       - Server Failure
                       - Cache Failure
                       - General Failure

--- lib/application/pages/advice/bloc/adviser_state.dart
+++ lib/application/pages/advice/bloc/adviser_state.dart
@@ -1,17 +1,33 @@
 part of 'adviser_bloc.dart';
 
-abstract class AdviserState {}
+abstract class AdviserState extends Equatable{}
 
-class AdviserInitial extends AdviserState {}
+class AdviserInitial extends AdviserState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
 
-class AdviserStateLoading extends AdviserState {}
+class AdviserStateLoading extends AdviserState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
 
 class AdviserStateLoaded extends AdviserState {
   final String advice;
   AdviserStateLoaded(this.advice);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props =>[];
 }
 
 class AdviserStateError extends AdviserState {
   final String errorMessage;
   AdviserStateError(this.errorMessage);
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
